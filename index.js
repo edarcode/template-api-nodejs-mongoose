25dc21bd2ff6703db7e2be71d5af304bcc38c0b1
edarcode <edarcode@gmail.com>
2022-09-20 10:24:29 -0500

    refactor

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ import { connDb } from "./src/connDb.js";
 import { PORT } from "./src/env/server.js";
 import { server } from "./src/server.js";
 
-connDb
-	.then(() => {
-		server.listen(PORT, () => {
-			console.log(`Server running on port: ${PORT} 😎`); // eslint-disable-line no-console
-		});
-	})
-	.catch(err => console.log(err));
+const start = async () => {
+	try {
+		await connDb;
+		server.listen(PORT, () =>
+			console.log(`Server running on port: ${PORT} 😎`)
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
